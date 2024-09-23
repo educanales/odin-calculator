73a6bf3f346731bb99b224d1ef2978016ec7e300
Edu Canales <131183212+educanales@users.noreply.github.com>
2024-09-23 14:30:30 -0300

    Add operate function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
 function add(firstNum, secondNum) {
   return firstNum + secondNum;
 }
@@ -14,7 +18,23 @@ function divide(firstNum, secondNum) {
   return firstNum / secondNum;
 }
 
-console.log(add(2, 2))
-console.log(subtrack(5, 3))
-console.log(multiply(4, 4))
-console.log(divide(10, 2))
+function operate(operator, a, b) {
+  switch (operator) {
+    case 'sum':
+      return add(a, b);
+    case 'subtrack':
+      return subtrack(a, b);
+    case 'multiply':
+      return multiply(a, b);
+    case 'divide':
+      return divide(a, b);
+  }
+}
+
+// console.log(add(2, 2))
+// console.log(subtrack(5, 3))
+// console.log(multiply(4, 4))
+// console.log(divide(10, 2))
+
+console.log(operate('sum', 2, 2))
+console.log(operate('divide', 10, 2))
