d1d84b25c81065acabee4c270492cd18a5a16fa8
Edu Canales <131183212+educanales@users.noreply.github.com>
2024-09-23 14:18:13 -0300

    Add basic math functions

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(firstNum, secondNum) {
+  return firstNum + secondNum;
+}
+
+function subtrack(firstNum, secondNum) {
+  return firstNum - secondNum;
+}
+
+function multiply(firstNum, secondNum) {
+  return firstNum * secondNum;
+}
+
+function divide(firstNum, secondNum) {
+  return firstNum / secondNum;
+}
+
+console.log(add(2, 2))
+console.log(subtrack(5, 3))
+console.log(multiply(4, 4))
+console.log(divide(10, 2))
