68b76d1cd04d7fc4bb3a0d097a32b1aed08d10c0
Edu Canales <131183212+educanales@users.noreply.github.com>
2024-09-24 12:57:03 -0300

    Add selectors for number buttons and create functions to display them on the screen

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let firstNumber;
 let secondNumber;
 let operator;
 
+const btnNum = document.querySelectorAll('.btn-num');
+const display = document.querySelector('.display')
+
 function add(firstNum, secondNum) {
   return firstNum + secondNum;
 }
@@ -31,10 +34,24 @@ function operate(operator, a, b) {
   }
 }
 
+btnNum.forEach((button) => {
+  button.addEventListener('click', () => {
+    // displayValue = button.id
+    displayBtn(button);
+  })
+})
+
+function displayBtn(info) {
+  displayValue = info.id
+  // console.log(typeof displayValue)
+  display.textContent = displayValue;
+}
+
+
 // console.log(add(2, 2))
 // console.log(subtrack(5, 3))
 // console.log(multiply(4, 4))
 // console.log(divide(10, 2))
 
-console.log(operate('sum', 2, 2))
-console.log(operate('divide', 10, 2))
+// console.log(operate('sum', 2, 2))
+// console.log(operate('divide', 10, 2))
