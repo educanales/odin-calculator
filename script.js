2b252bbaec26865bb3663dfce4c04dd91eaf6c77
Edu Canales <131183212+educanales@users.noreply.github.com>
2024-10-24 14:51:45 -0300

    Add event listener to numeric keys

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let firstNumber;
 let secondNumber;
-let operator = '';
-let displayValue = '';
+let operator = "";
+let displayValue = "";
 
-console.log(operator)
+console.log(operator);
 
-const btnNum = document.querySelectorAll('.btn-num');
-const display = document.querySelector('.display');
-const clearBtn = document.querySelector('.clear-btn');
-const equalBtn = document.querySelector('.equal-btn');
-const operatorBtn = document.querySelectorAll('.operator-btn');
-const delBtn = document.querySelector('.del-btn');
+const btnNum = document.querySelectorAll(".btn-num");
+const display = document.querySelector(".display");
+const clearBtn = document.querySelector(".clear-btn");
+const equalBtn = document.querySelector(".equal-btn");
+const operatorBtn = document.querySelectorAll(".operator-btn");
+const delBtn = document.querySelector(".del-btn");
 
 function add(firstNum, secondNum) {
   return firstNum + secondNum;
@@ -26,77 +26,106 @@ function multiply(firstNum, secondNum) {
 
 function divide(firstNum, secondNum) {
   if (secondNum === 0) {
-    return "Error"
+    return "Error";
   }
   return firstNum / secondNum;
 }
 
 function operate(operator, a, b) {
   switch (operator) {
-    case 'sum':
+    case "sum":
       return add(a, b);
-    case 'subtrack':
+    case "subtrack":
       return subtrack(a, b);
-    case 'multiply':
+    case "multiply":
       return multiply(a, b);
-    case 'divide':
+    case "divide":
       return divide(a, b);
   }
 }
 
 btnNum.forEach((button) => {
-  button.addEventListener('click', () => {
+  button.addEventListener("click", () => {
     let secondValue;
-    if (displayValue === '') {
+    if (displayValue === "") {
       displayValue = button.id;
     } else {
       secondValue = button.id;
       displayValue += secondValue;
     }
     if (displayValue.length >= 20) {
-      displayValue = displayValue.slice(0, 20)
+      displayValue = displayValue.slice(0, 20);
     } else {
       display.textContent = displayValue;
     }
-  })
-})
+  });
+});
 
-clearBtn.addEventListener('click', () => {
-  firstNumber = '';
-  secondNumber = '';
-  displayValue = '';
-  operator = '';
-  display.textContent = ''
-})
+window.addEventListener("keydown", (event) => {
+  // console.log(typeof(event.key))
+  if (
+    event.key === "0" ||
+    event.key === "1" ||
+    event.key === "2" ||
+    event.key === "3" ||
+    event.key === "4" ||
+    event.key === "5" ||
+    event.key === "6" ||
+    event.key === "7" ||
+    event.key === "8" ||
+    event.key === "9"
+  ) {
+    let secondValue;
+    if (displayValue === "") {
+      displayValue = event.key;
+    } else {
+      secondValue = event.key;
+      displayValue += secondValue;
+    }
+    if (displayValue.length >= 20) {
+      displayValue = displayValue.slice(0, 20);
+    } else {
+      display.textContent = displayValue;
+    }
+  }
+});
+
+clearBtn.addEventListener("click", () => {
+  firstNumber = "";
+  secondNumber = "";
+  displayValue = "";
+  operator = "";
+  display.textContent = "";
+});
 
 operatorBtn.forEach((button) => {
-  button.addEventListener('click', () => {
-    if (operator !== '') {
+  button.addEventListener("click", () => {
+    if (operator !== "") {
       getResult();
     }
     firstNumber = Number(displayValue);
-    displayValue = '';
-    
+    displayValue = "";
+
     switch (button.id) {
-      case '+':
-        operator = 'sum';
+      case "+":
+        operator = "sum";
         break;
-      case '-':
-        operator = 'subtrack';
+      case "-":
+        operator = "subtrack";
         break;
-      case '*':
-        operator = 'multiply';
+      case "*":
+        operator = "multiply";
         break;
-      case '/':
-        operator = 'divide';
+      case "/":
+        operator = "divide";
         break;
-    }    
+    }
     // displayValue += ` ${button.id} `;
     // display.textContent = displayValue;
-  })
-})
+  });
+});
 
-equalBtn.addEventListener('click', getResult);
+equalBtn.addEventListener("click", getResult);
 
 function getResult() {
   let result;
@@ -105,22 +134,22 @@ function getResult() {
   result = operate(operator, firstNumber, secondNumber);
   if (result % 1 !== 0) {
     result = result.toFixed(4);
-    console.log(typeof result)
+    console.log(typeof result);
   } else {
     result = result.toString();
-  }  
+  }
   // if (result.length >= 17) {
   //   console.log("exceed length")
   //   result = result.slice(0, 17);
   // }
   displayValue = result;
   firstNumber = result;
-  secondNumber = '';
-  operator = '';
+  secondNumber = "";
+  operator = "";
   display.textContent = result;
 }
 
-delBtn.addEventListener('click', () => {
+delBtn.addEventListener("click", () => {
   displayValue = displayValue.slice(0, -1);
   display.textContent = displayValue;
-})
+});
